88f58cf53b62dc052177ae8348a1b3ab42b96e4e
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-01-09 19:29:08 +0530

    Update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,14 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="sticky top-0 shadow">
-      <nav className="bg-white border-gray-200 px-4 py-2.5">
-        <ul className="flex m-4 font-medium">
-          <li className="p-1 px-8">
+    <header className="sticky top-0 shadow w-full">
+      <nav className="bg-white border-gray-200 text-lg">
+        <ul className="flex font-medium">
+          <li className="m-3">
             <NavLink
               to="/React-Router-Crash-Course/"
               className={({ isActive }) =>
-                `block py-2 pr-4 pl-3 duration-200 ${
+                `block py-2 duration-200 ${
                   isActive ? "text-orange-700" : "text-gray-700"
                 } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
               }
@@ -19,11 +19,11 @@ function Header() {
             </NavLink>
           </li>
 
-          <li className="p-1 px-8">
+          <li className="m-3">
             <NavLink
               to="/React-Router-Crash-Course/about"
               className={({ isActive }) =>
-                `block py-2 pr-4 pl-3 duration-200 ${
+                `block py-2  duration-200 ${
                   isActive ? "text-orange-700" : "text-gray-700"
                 } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
               }
@@ -32,11 +32,11 @@ function Header() {
             </NavLink>
           </li>
 
-          <li className="p-1 px-8">
+          <li className="m-3">
             <NavLink
               to="/React-Router-Crash-Course/contact"
               className={({ isActive }) =>
-                `block py-2 pr-4 pl-3 duration-200 ${
+                `block py-2  duration-200 ${
                   isActive ? "text-orange-700" : "text-gray-700"
                 } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
               }
@@ -45,11 +45,11 @@ function Header() {
             </NavLink>
           </li>
 
-          <li className="p-1 px-8">
+          <li className="m-3">
             <NavLink
               to="/React-Router-Crash-Course/githubuserdata"
               className={({ isActive }) =>
-                `block py-2 pr-4 pl-3 duration-200 ${
+                `block py-2  duration-200 ${
                   isActive ? "text-orange-700" : "text-gray-700"
                 } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
               }
